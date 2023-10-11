00c57c38d00ec9fd0a3d3154756a4fcb7ea9404d
raavimanikanta <92731330+raavimanikanta@users.noreply.github.com>
2023-10-11 18:42:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "newrelic_synthetics_monitor" "monitor" {
+  status           = "ENABLED"
+  name             = local.name
+  period           = "EVERY_MINUTE"
+  uri              = "https://www.one.newrelic.com"
+  type             = "SIMPLE"
+  locations_public = ["AP_SOUTH_1"]
+
+  custom_header {
+    name  = "some_name"
+    value = "some_value"                              
+  }
+
+  treat_redirect_as_failure = true
+  validation_string         = "success"
+  bypass_head_request       = true
+  verify_ssl                = true                                 
+
+  tag {
+    key    = "some_key"
+    values = ["some_value"]
+  }
+}
